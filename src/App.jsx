989e7c9ef512cdee87417e5feff8b2ecd740f264
Gabriel Mendez <mendez.gabriel.d@hotmail.com>
2023-12-12 14:13:36 -0300

    comienzo de portafolios

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import NavBar from "./Components/Specific/navBar/NavBar"
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+    <NavBar />
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
